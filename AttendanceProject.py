a178ae663faaba4a98737331c478a0e9a6e68dbf
Joanna <joanna@wloskowicz.pl>
2020-10-23 21:31:49 +0200

    final commit

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -2,6 +2,8 @@ import numpy as np
 import face_recognition
 import cv2
 import os
+from datetime import datetime
+
 
 
 path = 'ImageAttendance'
@@ -19,9 +21,23 @@ def findEncodings(images):
     encodeList = []
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode =  face_recognition.face_encodings(img)[0]
+        encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
+
+def markAttendance (name):
+    with open ('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
 
 encodelistKnown = findEncodings(images)
 
@@ -45,7 +61,14 @@ while True:
         if matches[matchIndex]:
             name = className[matchIndex].upper()
             print(name)
+            y1,x2,y2,x1 = faceLoc
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img, (x1,y2 - 35),(x2,y2),(0,255,0))
+            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2)
+            markAttendance(name)
 
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
 
 
 
